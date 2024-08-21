fdbb33f5ae879853b13597b8390a72d2f602e5be
Reginaldo16 <Reginaldotaylor16@gmail.com>
2024-08-22 01:43:53 +0200

    fix: header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,11 +8,11 @@ const Header = () => {
   };
 
   const menuItems = [
-    { name: "Home", href: "/" },
-    { name: "About", href: "/" },
-    { name: "Skills", href: "/" },
-    { name: "Projects", href: "/" },
-    { name: "Contact", href: "/" },
+    { name: "Pagina inicial", href: "/" },
+    { name: "sobre", href: "/" },
+    { name: "Habilidades", href: "/" },
+    { name: "Projectos", href: "/" },
+    { name: "Contacto", href: "/" },
   ];
 
   return (
